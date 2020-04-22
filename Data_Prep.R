6dfd69c3ad143c9cda5e6bc4b621abbab4232e30
Sam Powers <35611153+sam-powers@users.noreply.github.com>
2020-04-22 01:44:18 -0400

    Finish Data Cleaning and Writing output

---
+++ Data_Prep.R
@@ -0,0 +1,336 @@
+library(openxlsx)
+library(tidyverse)
+
+
+setwd("/Volumes/GoogleDrive/My Drive/QJL/ACLU COVID Investigation")
+
+current_pop <- read_csv("Data Files/jail_pop_cleaned.csv") %>% select(-X1)
+
+laplata <- read_csv("Data Files/Completed/La Plata.csv") %>%
+  mutate(Location = "La Plata",
+         Percent = round(Count/Total*100,1) )
+
+jan_pop <- read_csv("Data Files/HB19-1297Data .csv")
+
+
+#####
+# Who is  Over 65? 
+
+
+
+
+###################
+# Calculate Demographic Percentages for Each Jail
+
+levels(as.factor(current_pop$Ethnicity))
+
+current_pop %>%
+  filter(Ethnicity %in% c("AFRICAN", "ASIAN", "FUGITIVE FROM JUSTICE", "WHITE"))
+
+current_pop %>% 
+  filter(Race %in% c("C", "P", "S", "H")) #%>% View()
+
+
+levels(as.factor(current_pop$Race))
+
+Denver <-
+  cleaned_current_pop %>%
+  filter(Location == "Denver")
+Denver$Race
+
+
+cleaned_current_pop <- 
+current_pop %>% 
+  mutate(
+    
+    Over60 = ifelse(Age >= 60, "60+", "<60"),
+
+    Ethnicity = case_when(
+      is.na(Ethnicity) == TRUE ~ "UNKNOWN",
+      Ethnicity == "UNKNOWN"  ~ "UNKNOWN",
+      Ethnicity == "U"  ~ "UNKNOWN",
+      Ethnicity == "FUGITIVE FROM JUSTICE" ~ "UNKNOWN",
+      Ethnicity == "UNKNOW N" ~ "UNKNOWN",
+      Ethnicity == "Unknown" ~ "UNKNOWN",
+      Ethnicity == "NULL" ~ "UNKNOWN",
+      Ethnicity == "NULL" ~ "UNKNOWN",
+      Ethnicity == "WHITE" ~ "UNKNOWN",
+      Ethnicity == "P" ~ "UNKNOWN",
+      Ethnicity == "S" ~ "UNKNOWN",
+
+      Ethnicity == "AFRICAN" ~ "NON-HISPANIC",
+      Ethnicity == "ASIAN" ~ "NON-HISPANIC",
+      Ethnicity == "Non-Hispanic" ~ "NON-HISPANIC",
+      Ethnicity == "OINVUSIH-NON" ~ "NON-HISPANIC",
+      Ethnicity == "N" ~ "NON-HISPANIC",
+      Ethnicity == "NON-HISPANIC" ~ "NON-HISPANIC",
+
+      Ethnicity == "HISPANIC" ~ "HISPANIC",
+      Ethnicity == "DINVASIH" ~ "HISPANIC",
+      Ethnicity == "H" ~ "HISPANIC",
+      Ethnicity == "HISPANIC/LATINO" ~ "HISPANIC",
+      Ethnicity == "HISPANIĆ" ~ "HISPANIC",
+      Ethnicity == "Hispanic" ~ "HISPANIC"
+    ),
+    
+    Race = case_when(
+      
+      is.na(Race) == TRUE ~ "UNKNOWN",
+      Race == "UNKNOWN" ~ "UNKNOWN",
+      Race == "NULL" ~ "UNKNOWN",
+      Race == "U" ~ "UNKNOWN",
+      Race == "Unknown" ~ "UNKNOWN",
+      
+      Race == "NATIVE AMERICAN" ~ "NATIVE AMERICAN",
+      Race == "AMER INDIAN/ALASKAN" ~ "NATIVE AMERICAN",
+      Race == "American Indian, Alaskan Native" ~ "NATIVE AMERICAN",
+      Race == "AMERICAN INDIAN/ALASKAN NATIVE" ~ "NATIVE AMERICAN",
+      Race == "I" ~ "NATIVE AMERICAN",
+      Race == "INDIAN" ~ "NATIVE AMERICAN",
+      Race == "INDIAN/ALASKA NATIVE" ~ "NATIVE AMERICAN",
+      
+      Race == "OTHER" ~ "OTHER",
+      Race == "A" ~ "OTHER",
+      Race == "ASIAN" ~ "OTHER",
+      Race == "Asian / Pacific Islander" ~ "OTHER",
+      Race == "ASIAN OR PACIFIC ISLANDER" ~ "OTHER",
+      Race == "ASIAN PACIFIC ISLANDER" ~ "OTHER",
+      
+      Race == "WHITE" ~ "WHITE",
+      Race == "C" ~ "WHITE",
+      Race == "H" ~ "WHITE",
+      Race == "W" ~ "WHITE",
+      Race == "W   " ~ "WHITE",
+      Race == "W HITE  " ~ "WHITE",
+      Race == "White" ~ "WHITE",
+      Race == "White  " ~ "WHITE",
+      Race == "WHITE\n-" ~ "WHITE",
+      Race == "WHITE." ~ "WHITE",
+      
+      Race == "BLACK" ~ "BLACK",
+      Race == "B" ~ "BLACK",
+      Race == "Black" ~ "BLACK",
+      Race == "BLACK/AFRICAN AMER" ~ "BLACK",
+      Race == "BLACK/AFRICAN AMERICAN" ~ "BLACK"
+     )
+ )
+  
+
+current_stats <- 
+cleaned_current_pop %>%
+  select(-c(DOB, `Last Name`, Age))  %>%
+  gather(Demographic, Level, -Location) %>%
+  group_by(Location, Demographic) %>%
+  add_count() %>%
+  group_by(Location, Demographic,  Level) %>%
+  summarize(Total = mean(n), Count = n()) %>%
+  mutate(Percent = round(Count/Total*100,1)) %>%
+  bind_rows(
+    laplata
+  ) 
+
+
+
+##########
+counties <- levels(as.factor(current_stats$Location))
+
+jan_pop_stats <-
+jan_pop %>%
+  filter(Measure %in% c("Average Daily Population", "Number of inmates") ) %>%
+  select(County, 
+         Measure,
+         Race_BLACK =  Black, 
+         `Race_NATIVE AMERICAN` = `Native American`, 
+         Race_OTHER = `Other Race`,
+         Race_WHITE = White,
+         Race_UNKNOWN = `Unknown Race`,
+         `Ethnicity_NON-HISPANIC` = `Non-Hispanic`,
+         Ethnicity_HISPANIC = Hispanic,
+         Ethnicity_UNKNOWN = `Unknown Ethnicity`
+         ) %>%
+  filter(County %in% counties) %>%
+  gather(Demographic, Count, -c(County, Measure) ) %>%
+  separate(Demographic, c("Demographic", "Level"), sep = "_") %>%
+  arrange(County, Demographic, Measure) %>% 
+  group_by(County, Demographic, Measure) %>%
+  mutate(Total = sum(Count)) %>%
+  filter(Total > 0) %>%
+  ungroup() %>%
+  select(-Total) %>%
+  spread(Measure, Count) %>%
+  rename(Count = `Average Daily Population`) %>%
+  mutate(Count = ifelse(is.na(Count), `Number of inmates`, Count)) %>%
+  select(Location = County, Demographic, Level, Count) %>%
+  group_by(Location, Demographic) %>%
+  mutate(Total = sum(Count)) %>%
+  ungroup() %>%
+  mutate(Percent = round(Count/Total*100,1)) %>%
+  mutate(Time = "January")
+  
+jan_pop_stats
+
+
+##########
+race_ethnicity_current <- 
+  current_stats %>%
+  filter(!Demographic %in% c("Over60")) %>%
+  mutate(Time = "April")
+
+
+final_stats <- 
+  race_ethnicity_current  %>%
+  bind_rows(jan_pop_stats) %>%
+  mutate(order = ifelse(Time == "April", 2, 1),
+         order2 = 
+           case_when(
+             Level == "HISPANIC" ~ 1,
+             Level == "NON-HISPANIC" ~ 2,
+             Demographic == "Ethnicity" & Level == "UNKNOWN" ~ 3,
+             
+             Level == "WHITE" ~ 1,
+             Level == "BLACK" ~ 2,
+             Level == "NATIVE AMERICAN" ~ 3,
+             Level == "OTHER" ~ 4,
+             Demographic == "Race" & Level == "UNKNOWN" ~ 5
+           ))
+
+final_stats_printout <-
+   final_stats %>%
+  ungroup() %>%
+   mutate(Display = paste0(Percent, "% (", Count, ")")) %>%
+  select(Location, Demographic, Level, Display, Time) %>% 
+  spread(Time, Display) %>%
+  select(Location, Demographic, Level, January, April)
+
+############
+over60_numbers <-  
+  current_stats %>%
+  filter(Demographic %in% c("Over60")) 
+
+
+above60 <- 
+  cleaned_current_pop %>% 
+  filter(Age >= 60)  %>%
+  select(`Last Name`, DOB, Race, Ethnicity, Age, Location)
+
+
+write.csv(above60 , file = "Results/popabove60.csv")
+
+#############
+
+final_stats
+
+
+  
+
+
+
+  
+wb <- createWorkbook()
+
+for (location in levels(as.factor(final_stats$Location )[c(7:8)] )) {
+  
+  sheetname <- location 
+  addWorksheet(wb, sheetname, gridLines = FALSE)
+  
+  over60output <-
+    over60_numbers %>%
+    filter(Location == location, Level == "60+")
+  
+  above60location <- above60 %>%
+    filter(Location == location) %>%
+    select(-Location)
+  
+  num60 <- paste0("There are ", over60output$Count, " people in ", location, " jail aged 60 and over" )
+  percent60 <- paste0("This comprises ", over60output$Percent, "% of the jail population")
+  
+  writeData(wb, sheetname, sheetname, startCol = 2, startRow = 1)
+  writeData(wb, sheetname, num60, startCol = 2, startRow = 3)
+  writeData(wb, sheetname, percent60, startCol = 2, startRow = 4)
+  writeData(wb, sheetname, "Those people are listed here:", startCol = 2, startRow = 6)
+  writeData(wb, sheetname, above60location, startCol = 2, startRow = 8)
+
+#################### 
+  graph_data_race  <- 
+     final_stats %>% 
+     filter(Location == location, Demographic == "Race")  
+  
+p1<-  
+  ggplot( graph_data_race , aes(x = reorder(Level, order2), y = Percent, fill = reorder(Time, order)) ) + 
+    geom_bar(stat = "identity", position = "dodge", alpha = .8, color = "black") +
+    ggtitle(paste0("Differences in Race Between January and April in ", location) )+
+    theme(
+      plot.title = element_text( face="bold", hjust = 0.5 ),
+      legend.title = element_text(),
+      axis.title.x = element_text(vjust=-.5),
+      axis.title.y = element_text(vjust= 2),
+      axis.text.x=element_text(vjust=-2),
+      axis.ticks.x = element_blank()
+      
+    )+ 
+    scale_y_continuous(labels = function(x) paste0(round(x), "%"), expand = c(0, 0 ), limits = c(-1, max(graph_data_race$Percent) + 5)) +
+    geom_text(aes(label= paste0(round(Percent), "% (", round(Count), ")"), y = Percent + max(graph_data_race$Percent)/50), position = position_dodge(width = .9), size = 3  ) +
+ #   geom_text(aes(label= paste0( "(n=", Total,")"), y = 0), position = position_dodge(width = .9), vjust = 1.5 ) +
+    labs(x= "Race", y="Proportion of Jail Population, %", fill = "")  +
+    coord_cartesian(clip = 'off')
+  
+     
+###########  
+
+
+graph_data_ethnicity  <- 
+  final_stats %>% 
+  filter(Location == location, Demographic == "Ethnicity")  
+
+p2 <- 
+ggplot( graph_data_ethnicity, aes(x = reorder(Level, order2), y = Percent, fill = reorder(Time, order)) ) + 
+  geom_bar(stat = "identity", position = "dodge", alpha = .8, color = "black") +
+  ggtitle(paste0("Differences in Ethnicity Between January and April in ", location) )+
+  theme(
+    plot.title = element_text( face="bold", hjust = 0.5 ),
+    legend.title = element_text(),
+    axis.title.x = element_text(vjust=-.5),
+    axis.title.y = element_text(vjust= 2),
+    axis.text.x=element_text(vjust=-2),
+    axis.ticks.x = element_blank()
+    
+  )+ 
+  scale_y_continuous(labels = function(x) paste0(round(x), "%"), expand = c(0, 0 ), limits = c(-1, max(graph_data_ethnicity$Percent) + 5)) +
+  geom_text(aes(label= paste0(round(Percent), "% (", round(Count), ")"), y = Percent + max(graph_data_ethnicity$Percent)/50), position = position_dodge(width = .9), size = 3  ) +
+#  geom_text(aes(label= paste0( "(n=", Total,")"), y = 0), position = position_dodge(width = .9), vjust = 1.5 ) +
+  labs(x= "Ethnicity", y="Proportion of Jail Population, %", fill = "")  +
+  coord_cartesian(clip = 'off')
+
+
+
+########
+jail_pops<-
+graph_data_ethnicity[,c(2,4,7)] %>% unique() %>%
+  bind_rows(
+    graph_data_race[,c(2,4,7)] %>% unique()
+  )  %>%
+  spread(Time, Total) 
+  
+
+jail_pop <- paste0("In January, there were about ", 
+                   max(jail_pops$January), " in ", 
+                   location, " jail, while in April, there were about ",  
+                   max(jail_pops$April))
+
+writeData(wb, sheetname, jail_pop, startCol = 9, startRow = 2)
+
+print(p1)
+insertPlot(wb, sheetname, xy = c("S", 4), width = 10, height = 5,  fileType = "png", units = "in")
+
+print(p2)
+insertPlot(wb, sheetname, xy = c("H", 4), width = 7.5, height = 5,  fileType = "png", units = "in")
+  
+}
+
+
+
+saveWorkbook(wb, "Results/COVIDTracking.xlsx", overwrite = TRUE)
+
+
+
+
--- PDF_Clean_Up.R
+++ PDF_Clean_Up.R
@@ -1,13 +1,22 @@
 library(pdftools)
-library(tidyverse)
 library(janitor)
 library(eeptools)
+library(openxlsx)
+library(tesseract)
+library(magick)
+
+library(tidyverse)
+
+
 
 # setwd("/Volumes/GoogleDrive/My Drive/QJL/ACLU COVID Investigation")
 
-arapahoe_file <- "Data Files/Completed/Arapahoe.pdf"
+####################
+# Arapahoe 
 
-arapahoe_pdf_structure <- pdf_data(arapahoe_file)
+arapahoe_file <- "Data Files/Completed/Arapahoe.pdf" # The File
+
+arapahoe_pdf_structure <- pdf_data(arapahoe_file)    # Get coordinates of data in pdf
 
  arapahoe_pages <- 
    lapply( 1:length(arapahoe_pdf_structure), function (x) {
@@ -16,15 +25,16 @@ arapahoe_pdf_structure <- pdf_data(arapahoe_file)
       arrange(x, y) %>%
       pivot_wider(id_cols = y, names_from = x, values_from = text) %>%
       arrange(y) %>% 
-      separate(`18`, c("Indicator", NA), 1)  %>%
-      filter(Indicator == "2")  %>%
+      separate(`18`, c("Indicator", NA), 1)  %>%      # Seperate out the first letter in the first column
+      filter(Indicator == "2")  %>%                   # Only rows with IDs of ppl should have 2's at the front
       rename(`Last Name` = `147`) %>%
       rename(DOB = `326`) %>%
       unite(col = "Gender", `426`:`427`, sep = "", remove = TRUE, na.rm = TRUE) %>%
        unite(col = "Race", `378`:`398`, sep = "", remove = TRUE, na.rm = TRUE) %>%  
        unite(col = "Ethnicity", `450`:`507`, sep = "", remove = TRUE, na.rm = TRUE) %>%  
        select(`Last Name`, DOB, Race, Ethnicity ) 
-  }
+   }
+  
 )
  
 
@@ -36,7 +46,9 @@ arapahoe <- do.call(rbind, arapahoe_pages) %>%
                         as.Date("2020-04-20"), 
                         units = "years")
                        )
-                    ) 
+                    ) %>%
+  mutate(Location = "Arapahoe")
+
 
 arapahoe %>% View()
 
@@ -55,15 +67,15 @@ larimer_pdf_structure[[x]] %>%
   pivot_wider(id_cols = y, names_from = x, values_from = text) %>%  
   arrange(y) %>% 
   rename(Name = `12`) %>%
-  filter(!is.na(Name) | !is.na(lag(Name, 1)) | !is.na(lag(Name, 2))
+  filter(!is.na(Name) | !is.na(lag(Name, 1)) | !is.na(lag(Name, 2))   # If its a name or the two rows below it, keep it. 
          ) %>% 
   select(y, Name, DOB = `180`, Race = `273`, Ethnicity = `327`)  %>% 
-  mutate(indicator = ceiling((1:n())/3  )) %>%
+  mutate(indicator = ceiling((1:n())/3  )) %>%                        # Make an indicator for three row groups
   select(-y) %>%
-  gather( colnames ,values, -indicator) %>%
+  gather( colnames ,values, -indicator) %>%                           # gather so we can filter out the nas
   arrange(indicator) %>% 
-  filter(!is.na(values)) %>%
-  spread(colnames, values) %>%
+  filter(!is.na(values)) %>% 
+  spread(colnames, values) %>%                                        # put it all back together
   mutate(DOB = as.Date(DOB, "%m/%d/%Y"),
          Age = floor(
            age_calc(DOB,
@@ -77,9 +89,10 @@ larimer_pdf_structure[[x]] %>%
 })
  
 
-larimer_pages
 
-larimer <- do.call(rbind, larimer_pages)
+larimer <- do.call(rbind, larimer_pages) %>%
+  mutate(Location = "Larimer")
+
 
 larimer %>% View()
   
@@ -110,37 +123,38 @@ jefferson_pages <- lapply( 1:20, function (x) {
 )
 
 jefferson <- do.call(rbind, jefferson_pages) %>%
+  
   bind_rows(
 
-
-jefferson_pdf_structure[[21]] %>%
-  arrange(x, y) %>% 
-  pivot_wider(id_cols = y, names_from = x, values_from = text) %>%  
-  arrange(y) %>% 
-  rename(Name = `18`) %>%
-  filter(!is.na(Name)) %>% 
-  unite(col = "Gender", `198`:`199`, sep = "", remove = TRUE, na.rm = TRUE) %>% 
- add_column( d = "", .before = "255") %>%
-  unite(col = "Race", `221`:d, sep = "", remove = TRUE, na.rm = TRUE) %>% 
-  unite(col = "DOB", `255`:`271`, sep = "", remove = TRUE, na.rm = TRUE) %>%
-  select(Name, DOB, Gender, Race) %>%
-  slice(2:n()) %>%
-  filter(!Name %in% c("Booking_Name"))
-) %>%
+ jefferson_pdf_structure[[21]] %>%
+    arrange(x, y) %>% 
+    pivot_wider(id_cols = y, names_from = x, values_from = text) %>%  
+    arrange(y) %>% 
+    rename(Name = `18`) %>%
+    filter(!is.na(Name)) %>% 
+    unite(col = "Gender", `198`:`199`, sep = "", remove = TRUE, na.rm = TRUE) %>% 
+    add_column( d = "", .before = "255") %>%
+    unite(col = "Race", `221`:d, sep = "", remove = TRUE, na.rm = TRUE) %>% 
+    unite(col = "DOB", `255`:`271`, sep = "", remove = TRUE, na.rm = TRUE) %>%
+    select(Name, DOB, Gender, Race) %>%
+    slice(2:n()) %>%
+    filter(!Name %in% c("Booking_Name"))
+  ) %>%
+  
   bind_rows(
 
   jefferson_pdf_structure[[22]] %>%
-  arrange(x, y) %>% 
-  pivot_wider(id_cols = y, names_from = x, values_from = text) %>%  
-  arrange(y) %>% 
-  rename(Name = `18`) %>%
-  filter(!is.na(Name)) %>%  
-  unite(col = "Gender", `198`:`199`, sep = "", remove = TRUE, na.rm = TRUE)  %>%
-  add_column( d = "", .before = "254") %>%
-  unite(col = "Race", `221`:d, sep = "", remove = TRUE, na.rm = TRUE) %>% 
-  unite(col = "DOB", `254`:`271`, sep = "", remove = TRUE, na.rm = TRUE) %>%
-  select(Name, DOB, Gender, Race) %>%
-  slice(2:n()) 
+    arrange(x, y) %>% 
+    pivot_wider(id_cols = y, names_from = x, values_from = text) %>%  
+    arrange(y) %>% 
+    rename(Name = `18`) %>%
+    filter(!is.na(Name)) %>%  
+    unite(col = "Gender", `198`:`199`, sep = "", remove = TRUE, na.rm = TRUE)  %>%
+    add_column( d = "", .before = "254") %>%
+    unite(col = "Race", `221`:d, sep = "", remove = TRUE, na.rm = TRUE) %>% 
+    unite(col = "DOB", `254`:`271`, sep = "", remove = TRUE, na.rm = TRUE) %>%
+    select(Name, DOB, Gender, Race) %>%
+    slice(2:n()) 
   
   )  %>%
 
@@ -157,7 +171,9 @@ jefferson  %>%
   )  %>%
 separate(Name, c("Last Name", NA), sep = ",") %>%
   mutate(Ethnicity = NA) %>%
-  select(`Last Name`, DOB, Race, Ethnicity, Age)
+  select(`Last Name`, DOB, Race, Ethnicity, Age) %>%
+  mutate(Location = "Jefferson")
+
 
 
 jefferson %>% View()
@@ -180,7 +196,7 @@ douglas_pages <- lapply( 1:length(douglas_pdf_structure), function (x) {
     }
   )
 
-douglas_pages
+
 
 douglas <- do.call(rbind, douglas_pages) %>%
   mutate(Data = ifelse(Data == "", "UNKNOWN", Data)) %>%
@@ -212,7 +228,300 @@ douglas %>%
                 )   %>% 
 
   mutate(`Last Name` = NA) %>%
-  select(`Last Name`, DOB, Race, Ethnicity, Age)
+  select(`Last Name`, DOB, Race, Ethnicity, Age) %>%
+  mutate(Race = str_sub(Race,  1,-2),
+         Ethnicity = str_sub(Ethnicity, 1,  -2)) %>%
+  mutate(Location = "Douglas")
 
 
-douglas %>% View()
+
+# douglas %>% View()
+
+######################
+# Adams 1
+
+Adams1 <- read_csv("Data Files/Completed/Adams 1.csv")
+
+Adams1 <- 
+Adams1  %>%
+
+  mutate(DOB = as.Date(Age, "%m/%d/%Y") ,
+
+         Age = floor(
+           age_calc(DOB,
+                    as.Date("2020-04-20"), 
+                    units = "years")
+         )
+  ) %>%
+  separate(`Last First Middle Name`, c("Last Name", NA), sep = " ") %>%
+  select(`Last Name`, DOB, Race, Ethnicity, Age) %>%
+  mutate(Location = "Adams")
+
+  # Adams1 %>% View()
+
+#######################
+# El Paso
+
+elpaso <- read_csv("Data Files/Completed/El Paso.csv")
+
+elpaso <- 
+elpaso  %>%
+  group_by(BookingNo) %>%
+  slice(1) %>%
+  ungroup(BookingNo) %>%
+   separate(DOB, c("D", "M", "Y"), sep = "/") %>%
+   mutate( Y = ifelse(Y > 20, 
+                      paste0("19", Y),
+                      paste0("20", Y))
+   ) %>%
+  unite(col = "DOB", `D`:`Y`, sep = "/", remove = TRUE, na.rm = TRUE) %>%  
+     mutate(DOB = as.Date(DOB, "%m/%d/%Y") ,
+         
+            Age = floor(
+                  age_calc(DOB,
+                       as.Date("2020-04-20"), 
+                       units = "years")
+                       )
+          ) %>%
+    separate(N, c("Last Name", NA), sep = ",") %>%
+    select(`Last Name`, DOB, Race, Ethnicity, Age) %>%
+    mutate(Location = "El Paso")
+
+# elpaso
+
+#######################
+# Weld 
+
+weld_file <- read_csv("Data Files/Completed/Weld.csv")
+
+
+weld <- 
+  weld_file  %>%
+  separate(DOB, c("M", "D", "Y"), sep = "/") %>%
+  mutate( Y = ifelse(Y > 20, 
+                     paste0("19", Y),
+                     paste0("20", Y))
+  ) %>%
+  unite(col = "DOB", `M`:`Y`, sep = "/", remove = TRUE, na.rm = TRUE) %>%  
+  mutate(DOB = as.Date(DOB, "%m/%d/%Y") ,
+         
+         Age = floor(
+           age_calc(DOB,
+                    as.Date("2020-04-20"), 
+                    units = "years")
+         )
+  ) %>%
+  select(`Last Name`, DOB, Race, Ethnicity, Age) %>%
+  mutate(Location = "Weld")
+
+# weld
+
+##################
+# Boulder
+
+
+boulder_file <- read_csv("Data Files/Completed/Boulder.csv")
+
+boulder <- 
+  boulder_file  %>%
+  group_by(Name, Booked) %>%
+  slice(1) %>%
+  ungroup(Name, Booked) %>%
+  separate(Name, c("Last Name", NA), sep = ",") %>%
+  mutate(DOB = NA, Ethnicity = NA) %>%
+  select(`Last Name`, DOB, Race, Ethnicity, Age) %>%
+  mutate(Location = "Boulder")
+
+# boulder
+
+#######################
+# Mesa
+
+mesa_file <- read_csv("Data Files/Completed/Mesa.csv")
+
+mesa <- 
+  mesa_file  %>%
+  select(Name = `Booking Inmate Global Subject`, 
+         DOB = `Booking Inmate Global Subject Date Of Birth`, 
+         Ethnicity = `Booking Inmate Global Subject Ethnicity`,
+         Race = `Booking Inmate Global Subject Race`) %>%
+  group_by(Name, DOB) %>%
+  slice(1) %>%
+  ungroup() %>%
+
+  mutate(DOB = as.Date(DOB, "%m/%d/%Y") ,
+         
+         Age = floor(
+           age_calc(DOB,
+                    as.Date("2020-04-20"), 
+                    units = "years")
+         )
+  ) %>%
+  
+  separate(Name, c("First Name", "Middle Name", "Last Name"), sep = " " , fill = "left", extra = "merge") %>%
+  select(`Last Name`, DOB, Race, Ethnicity, Age) %>%
+  mutate(Location = "Mesa")
+
+# mesa 
+
+
+#######################
+# Fremont
+
+fremont_file<- read_csv("Data Files/Completed/Fremont.csv")
+
+fremont <- 
+  fremont_file  %>%
+  group_by(InmateName, DOB) %>%
+  slice(1) %>%
+  ungroup() %>%
+  
+  separate(DOB, c("M", "D", "Y"), sep = "/") %>%
+  mutate( Y = ifelse(Y > 20, 
+                     paste0("19", Y),
+                     paste0("20", Y))
+  ) %>%
+  unite(col = "DOB", `M`:`Y`, sep = "/", remove = TRUE, na.rm = TRUE) %>%  
+  
+  mutate(
+        DOB = as.Date(DOB, "%m/%d/%Y") 
+          ) %>%
+  
+  separate(InmateName, c("Last Name", NA), sep = "," , extra = "drop") %>%
+  mutate(Ethnicity = NA) %>%
+  select(`Last Name`, DOB, Race, Ethnicity, Age) %>%
+  mutate(Location = "Fremont")
+
+# fremont 
+
+
+
+#######################
+# Denver
+
+denver_file <- read_csv("Data Files/Completed/Denver.csv")
+
+numbermonths <- data.frame(Month = c( "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
+                           M = 1:12)
+
+
+denver <- 
+  denver_file  %>%
+  group_by(`First Name`, `Last Name`, `Birth Date`) %>%
+  slice(1) %>%
+  ungroup() %>%
+  rename(DOB = `Birth Date`) %>%
+
+  separate(DOB, c("D", "Month", "Y"), sep = "-") %>%
+  inner_join(numbermonths) %>%
+  mutate( Y = ifelse(Y > 20, 
+                     paste0("19", Y),
+                     paste0("20", Y)),
+       
+  )   %>%
+  unite(col = "DOB", c("M", "D", "Y"), sep = "/", remove = TRUE, na.rm = TRUE)  %>%  
+  
+  mutate(
+      DOB = as.Date(DOB, "%m/%d/%Y"),
+    Age = floor(
+      age_calc(DOB,
+               as.Date("2020-04-20"), 
+               units = "years")
+    )
+    
+  ) %>%
+  
+  select(`Last Name`, DOB, Race, Ethnicity, Age) %>%
+  mutate(Location = "Denver")
+
+# denver 
+
+#####################
+# La Plata
+
+laplata <- read_csv("Data Files/Completed/La Plata.csv")
+
+######################
+# Logan
+
+# pdf_subset('Data Files/Problem Children/Logan.pdf',
+#           pages = 1, output = "Logan1.pdf")
+
+# pdf_subset('Data Files/Problem Children/Logan.pdf',
+#           pages = 2, output = "Logan2.pdf")
+
+logan_file <- read_csv("Data Files/Completed/Logan.csv")
+
+logan <-
+  logan_file %>%
+      separate(Name, c("First Name", "Middle Name", "Last Name"), sep = " " , fill = "left", extra = "merge") %>%
+      separate(DOB, c("DOB", NA), sep = " ") %>% 
+      separate(DOB, c("M", "D", "Y"), sep = "/") %>%
+      mutate( Y = ifelse(Y > 20, 
+                     paste0("19", Y),
+                     paste0("20", Y))
+              ) %>%
+      unite(col = "DOB", `M`:`Y`, sep = "/", remove = TRUE, na.rm = TRUE) %>% 
+      mutate(DOB = as.Date(DOB, "%m/%d/%Y") ,
+         
+            Age = floor(
+                 age_calc(DOB,
+                    as.Date("2020-04-20"), 
+                    units = "years")
+            )
+            )  %>%
+      select(`Last Name`, DOB, Race, Ethnicity = ETHNICITY, Age) %>%
+      mutate(Location = "Logan")
+
+
+
+
+
+
+######################
+# Washington
+
+# pdf_subset('Data Files/Problem Children/Washington.pdf',
+#           pages = 1, output = "Washington1.pdf")
+
+# pdf_subset('Data Files/Problem Children/Washington.pdf',
+#           pages = 2, output = "Washington2.pdf")
+
+# pdf_subset('Data Files/Problem Children/Washington.pdf',
+#           pages = 3, output = "Washington3.pdf")
+
+# pdf_subset('Data Files/Problem Children/Washington.pdf',
+#           pages = 4, output = "Washington4.pdf")
+
+
+washington_file <- read_csv("Data Files/Completed/Washington.csv")
+
+washington <- 
+washington_file %>%
+  separate(DOB, c("M", "D", "Y"), sep = "/") %>%
+  mutate( Y = ifelse(Y > 20, 
+                     paste0("19", Y),
+                     paste0("20", Y))
+        ) %>%
+  unite(col = "DOB", `M`:`Y`, sep = "/", remove = TRUE, na.rm = TRUE) %>% 
+  mutate(DOB = as.Date(DOB, "%m/%d/%Y") ,
+         
+         Age = floor(
+           age_calc(DOB,
+                    as.Date("2020-04-20"), 
+                    units = "years")
+         )
+  ) %>%
+  mutate(`Last Name` = NA) %>%
+  select(`Last Name`, DOB, Race, Ethnicity = `Ethnic Origin`, Age) %>%
+  mutate(Location = "Washington")
+
+##########################
+person_data <- 
+bind_rows(arapahoe, larimer, jefferson, douglas, Adams1, elpaso, weld, boulder, mesa, fremont, denver, logan, washington)
+
+
+write.csv(person_data, file = "Data Files/jail_pop_cleaned.csv")
+
+
+
